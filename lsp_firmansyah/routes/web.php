7595e121e99d972d2ff330f62883562f6eddcd4d
Firman <100285601+Pirmannnsyah@users.noreply.github.com>
2024-08-07 13:33:11 +0700

    Revert "sementara"
    This reverts commit 0dc864909c11ad9f97a4ccd79c9d1a30857168cb.

--- lsp_firmansyah/routes/web.php
+++ lsp_firmansyah/routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\PengumumanController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\DaftarmahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,19 +20,6 @@ Route::get('/alur', function () {
 Route::get('/biaya', function () {
     return view('biaya');
 });
-
-Route::get('/daftarmahasiswa', [DaftarmahasiswaController::class, 'index'])->name('daftarmahasiswa.index');
-Route::get('/daftarmahasiswa/create', [DaftarmahasiswaController::class, 'create'])->name('daftarmahasiswa.create');
-Route::post('/daftarmahasiswa', [DaftarmahasiswaController::class, 'store'])->name('daftarmahasiswa.store');
-Route::get('/daftarmahasiswa/{id}/edit', [DaftarmahasiswaController::class, 'edit'])->name('daftarmahasiswa.edit');
-Route::put('/daftarmahasiswa/{id}', [DaftarmahasiswaController::class, 'update'])->name('daftarmahasiswa.update');
-
-Route::get('/pengumuman', [PengumumanController::class,'index']);
-Route::get('/pengumuman/create', [PengumumanController::class,'create']);
-Route::post('/pengumuman/store', [PengumumanController::class,'store']);
-Route::get('/pengumuman/{id}/edit', [PengumumanController::class,'edit']);
-Route::put('/pengumuman/{id}', [PengumumanController::class,'update']);
-Route::delete('/pengumuman/{id}', [PengumumanController::class,'destroy']);
 
 Route::get('/navbar', function () {
     return view('layout/navbar');
